e9ddec129bcd7fc6fbd40549f5c3d2987b747cc4
soapmactavish23 <ricknogueira1231@gmail.com>
2024-12-02 19:02:51 -0300

    first commit

---
+++ src/main/java/com/algaworks/algafood_auth/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood_auth;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AlgafoodAuthApplication.class);
+	}
+
+}
